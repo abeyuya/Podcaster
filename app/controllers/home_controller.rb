59474e815c6b3d9c6556874a600e26c5246b2e2e
abeyuya <yuya.abe.0525@gmail.com>
2015-05-31 00:34:19 +0900

    create podcast contller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
+  # dashboard
   def index
-    @user = current_user
   end
 
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,5 +1,7 @@
 class Podcast < ActiveRecord::Base
 
+  enum media_type: %i(nothing sound_only video_only image_only sound_and_video)
+
   belongs_to :user
 
 end
