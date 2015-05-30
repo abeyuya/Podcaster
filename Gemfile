2b4ee12754b560d3e803808f87b36b7ed9c08b09
abeyuya <yuya.abe.0525@gmail.com>
2015-05-30 20:47:03 +0900

    introduce dotenv

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # User management
 gem 'devise'
 
+# ignore developer info
+gem 'dotenv-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,12 @@ Rails.application.configure do
 
   # for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :authentication => :plain,
+    :user_name      => ENV["GMAIL_USER_NAME"],
+    :password       => ENV["GMAIL_PASSWORD"]
+  }
 end
